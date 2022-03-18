0148712c0359a62e9e652819d728b5557c9baa8a
Nelli Efremyan <n.efremyan@gmail.com>
2022-03-18 13:47:40 +0300

    FormItem added

--- src/modal/Modal.jsx
+++ src/modal/Modal.jsx
@@ -1,25 +1,53 @@
 import React from 'react';
 import './modal.css';
 import { Routes, Route } from 'react-router-dom'
-
-import RegistrationForm from '../components/forms/registration/RegistrationForm';
-import LoginForm from '../components/forms/login/LoginForm';
-import ResetPasswordForm from '../components/forms/password/ResetPasswordForm';
+import FormItem from '../components/forms/FormItem';
 
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faXmark } from '@fortawesome/free-solid-svg-icons';
 
 
-function Modal({ active, setActive, children }) {
+function Modal({ active, setActive }) {
   return (
-    <div className={active ? 'modal active' : 'modal'}>
-      <div className={active ? 'modal_content active' : 'modal_content'} onClick={(e) => e.stopPropagation()}>
+    <div className={`modal ${active && 'active'}`}>
+      <div className={`modal_content ${active && 'active'}`} onClick={(e) => e.stopPropagation()}>
       <FontAwesomeIcon icon={faXmark} className="crossIcon" onClick={() => setActive(false)} />
         <Routes>
-          <Route path='/registration' element={<RegistrationForm />} />
-          <Route path='/login' element={<LoginForm />} />
-          <Route path='/reset-password' element={<ResetPasswordForm />} />
-          {children}
+          <Route 
+            path='/registration' 
+            element={
+              <FormItem 
+                title={'Регистрация'}
+                input={{email: 'Email', password: 'Пароль'}}
+                button={{submit: 'Зарегистрироваться', redirect: 'Войти'}}
+                infoText={'Регистрируясь на сайте, вы соглашаетесь с нашими Правилами и Политикой конфиденциальности и соглашаетесь на информационную рассылку.'}
+                link={'/login'}
+                formType={'registration'}
+              />} />
+          
+          <Route 
+            path='/login' 
+            element={
+              <FormItem
+                title={'Вход'}
+                input={{email: 'Email', password: 'Пароль'}}
+                button={{submit: 'Войти', redirect: 'Регистрация'}}
+                infoText={'Восстановить пароль'}
+                link={'/registration'}
+                formType={'login'}
+              />} />
+          
+          <Route 
+            path='/reset-password' 
+            element={
+              <FormItem 
+                title={'Восстановление пароля'}
+                input={{email: 'Email'}}
+                button={{submit: 'Отправить'}}
+                infoText={'Срок действия пароля 24 ч.'}
+                formType={'reset_password'}
+                infoTextAdditional={'Для получения временного пароля необходимо ввести email, указанный при регистрации'}
+              />} />
         </Routes>
       </div>
     </div>
